33adfce140c03ee373c1b8f5b1f2646be8d58165
poojapauskar <git.poojapauskar@gmail.com>
2017-07-10 11:03:31 +0530

    auto suggestion

---
+++ autocomplete-Files/Logic_Search.js
@@ -0,0 +1,59 @@
+﻿/*jslint  browser: true, white: true, plusplus: true */
+/*global $, countries */
+
+$(function() {
+    'use strict';
+// var countriesArray = $.map(kycc, function (value, key) { 
+//     return { value: value, data: key }; });
+var peopleArray = $.map(peoplenames, function (value, key) { 
+    return { value: value, data: key }; });
+// var countriesArray = $.map(countries, function (value) { 
+//         return {"pooja":"pooja"}; });
+
+// var countriesArray = $.grep(countries, function (value, id) { 
+//         return {value:value, data:id }};);
+
+    // Setup jQuery ajax mock:
+    $.mockjax({
+        url: '*',
+        responseTime: 2000,
+        response: function(settings) {
+            var query = settings.data.query,
+                queryLowerCase = query.toLowerCase(),
+                re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi'),
+                suggestions = $.grep(peopleArray, function(search) {
+                    // return country.value.toLowerCase().indexOf(queryLowerCase) === 0;
+                    return re.test(search.value);
+                }),
+                response = {
+                    query: query,
+                    suggestions: suggestions
+                };
+
+            this.responseText = JSON.stringify(response);
+        }
+    });
+
+
+
+    // Initialize autocomplete with custom appendTo:
+    $('#search').autocomplete({
+        lookup: peopleArray,
+        showNoSuggestionNotice:true,
+        noSuggestionNotice: "No results Found",
+         onSelect: function(suggestion)
+         {
+
+         /*  var wrapper= $("#tagname");
+          $(wrapper).append(''+suggestion.data+'');    
+*/      var value1= suggestion.data
+
+        /*document.getElementById("is_user_field").value = value1;*/
+        document.getElementById("id_field").value = value1;
+        document.getElementById('submit').click();
+
+        }
+
+
+    });
+});
--- home.php
+++ home.php
@@ -1,3 +1,9 @@
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="autocomplete-Files/styles.css">
+</head>
+<body>
+
 <?php 
 session_start();
 if($_SESSION['krishi_login']==1){
@@ -38,3 +44,86 @@ function set2(){
     </button>
 
 </form>
+
+<?php
+  /*echo $_POST['pk_value'];*/
+  $url = 'https://krishi-udyog.herokuapp.com/fetch_category_subcategory/';
+  $options = array(
+    'http' => array(
+      'method'  => 'GET',
+    ),
+  );
+  $context = stream_context_create($options);
+  $output = file_get_contents($url, false,$context);
+  $arr = json_decode($output,true);
+  /*echo count($arr);*/
+  
+?>
+
+<form enctype="multipart/form-data" action="produce.php" method="post" >
+  
+  <!-- <label style="margin-left:2%" class="form-label span3" for="file">Import Students</label><br><br>
+   -->
+   <!-- <label>User</label> -->
+   <input style="margin-top:2%" type="text" placeholder="Select User" name="user_id" id="user_id" required></input><br>
+   <input type="text" name="id_field" id="id_field"></input>
+   <!-- <label>Sub Category</label> -->
+   <br>
+   <select name="sub_category" id="sub_category" placeholder="Select Sub Category" required>
+    <?php for($i=0;$i<count($arr);$i++){ ?>
+     <option disabled="true"><?php echo $arr[$i]['Category']['c_type']; ?></option>
+     <?php for($j=0;$i<count($arr[$j]['Sub_categories']);$j++){ ?>
+      <?php if($arr[$j]['Sub_categories'][$j]['pk'] != ""){?>
+      <option value="<?php echo $arr[$j]['Sub_categories'][$j]['pk']; ?>"><?php echo $arr[$j]['Sub_categories'][$j]['sc_type']; ?></option>
+      <?php } ?>
+     <?php } ?>
+    <?php } ?>
+   </select>
+   <br>
+   <input style="margin-top:2%" type="file" name="file" id="file" required />
+  
+  
+  <br><br>
+  <button id="post_produce" class="btn btn-success" style="color:white;background-color:green;width:100px;height:40px" type="submit">
+    Post Produce
+    </button>
+
+</form>
+
+<?php
+
+session_start();
+
+fopen('autocomplete-Files/users.js', 'w');
+
+$db = pg_connect("host=ec2-50-17-220-223.compute-1.amazonaws.com port=5432 dbname=d78v66su68v3a9 user=rrmuwqbkpwcldy password=d6be3e788e178b0c4e3843d1f8c466f79c277d31cfbfe8b3b0ba7947c5d31177");
+ pg_select($db, 'post_log', $_POST);
+
+
+ $query=pg_query("SELECT id,name FROM userregistration_userregistration");
+
+/*echo $query;*/
+
+ $json=array();
+while ($student = pg_fetch_array($query)) {
+    $json[$student["id"]] = $student["name"];
+}
+
+$textval = json_encode($json);
+$foo = "var peoplenames=" . $textval;
+
+file_put_contents('autocomplete-Files/users.js', $foo);
+
+?>
+
+
+
+    <!-- AutoSearch Script files don't move -->
+     <script type="text/javascript" src="autocomplete-Files/jquery-1.8.2.min.js"></script>
+     <script type="text/javascript" src="autocomplete-Files/jquery.mockjax.js"></script>
+     <script type="text/javascript" src="autocomplete-Files/jquery.autocomplete.js"></script>
+    <script type="text/javascript" src="autocomplete-Files/Logic_Search.js"></script>
+        <script type="text/javascript" src="autocomplete-Files/users.js"></script> 
+
+</body>
+</html>
